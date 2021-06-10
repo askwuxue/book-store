b94aa5ef9942edcc18d053a2c68a7546ff779e41
askwuxue <972523419@qq.com>
2021-06-10 23:26:55 +0800

    use Routes Provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
+import Routes from './components/admin/Routes';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Routes></Routes>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
