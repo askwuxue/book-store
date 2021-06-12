34f45be527a9c465c653ab3bd93cd50a5609d5c9
askwuxue <972523419@qq.com>
2021-06-12 23:38:48 +0800

    use createRootReducer function

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,27 @@
 import { createStore } from "redux";
-import rootReducer from './reducers';
+// import rootReducer from './reducers';
+import { createBrowserHistory } from "history";
+import createRootReducer from "./reducers";
+import { applyMiddleware, compose } from "redux";
+import { routerMiddleware } from "connected-react-router";
 
-// 创建store
-const store = createStore(rootReducer);
+export const history = createBrowserHistory();
 
-export default store;
+export default function configureStore(preloadedState) {
+    const store = createStore(
+        createRootReducer(history),
+        preloadedState,
+        compose(
+            applyMiddleware(
+                routerMiddleware(history)
+            )
+        )
+    )
+    return store;
+}
+
+// 1. 创建store
+// const store = createStore(rootReducer);
+// 2. 导出全局使用的store 
+// export default store;
+
