78817b14f8fd71df8d1e084212a2e5fa534101fb
askwuxue <972523419@qq.com>
2021-06-12 23:41:56 +0800

    use antd

--- src/components/admin/core/Home.js
+++ src/components/admin/core/Home.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 import Layout from './Layout'
 
 export default function Home() {
     // TODO useSelector
-    const state = useSelector(state => state);
     return (
-        <Layout>
-            Home---state{JSON.stringify(state)}
-        </Layout>
+        <Layout title="精华书城" subTitle="好书尽在精华书城">
+        </Layout >
     )
 }
--- src/components/admin/core/Layout.js
+++ src/components/admin/core/Layout.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Navigation from './Navigation';
+import { PageHeader } from 'antd';
 
 export default function Layout(props) {
+    const { title, subTitle } = props;
     return (
         <div>
-            Layout {props.children}
-        </div>
+            {/* 导航 */}
+            <Navigation></Navigation>
+            {/* 页头 */}
+            <PageHeader className="jumbotron" title={title} subTitle={subTitle} />
+            <div style={{ width: '50%', margin: '0 auto' }}>
+                {props.children}
+            </div>
+        </div >
     )
 }
