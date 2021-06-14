aeb0f39cdd492f4dafb52596479562df82b97f01
askwuxue <972523419@qq.com>
2021-06-14 23:57:48 +0800

    use redux-actions create actions

--- src/store/actions/register.js
+++ src/store/actions/register.js
@@ -1,22 +1,12 @@
 import { createActions } from 'redux-actions';
 
-export const { register, registerSuccess, registerFailed } = createActions({
-    REGISTER: () => ({
-        loading: false,
-        loaded: false,
-        success: false,
-        message: ''
-    }),
-    REGISTERSUCCESS: ({
-        loading: false,
-        loaded: true,
-        success: true,
-        message: ''
-    }),
-    REGISTERFAILED: ({
-        loading: false,
-        loaded: true,
-        success: false,
-        message: 'register failed'
-    })
-})
+// export const { register, registerSuccess, registerFailed } = createActions({
+//     REGISTER: (value) => ({ ...value }),
+//     REGISTERSUCCESS: (value) => ({ ...value }),
+//     REGISTERFAILED: (value) => ({ ...value })
+// })
+
+// 创建action 参数为type
+export const register = createActions('REGISTER');
+export const registerSuccess = createActions('REGISTERSUCCESS');
+export const registerFailed = createActions('REGISTERFAILED');
