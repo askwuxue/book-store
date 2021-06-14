e03f7947418e7a5dc3d20ddf1b0d1a1556a00026
askwuxue <972523419@qq.com>
2021-06-14 23:56:46 +0800

    dispatch REGISTER action and pass param

--- src/components/admin/core/Register.js
+++ src/components/admin/core/Register.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
 import Layout from './Layout'
 import { Form, Input, Button, Checkbox } from 'antd';
+import { register } from '../../../store/actions/register';
 
 // 控制表单的样式
 const layout = {
@@ -19,10 +21,15 @@ const tailLayout = {
 };
 
 export default function Register() {
+    const dispatch = useDispatch();
+    const handleOnFish = value => {
+        // dispatch(register.register(value));
+        dispatch({ type: 'REGISTER', payload: value });
+    }
     return (
         <Layout title="注册" subTitle="">
-            <Form {...layout}>
-                <Form.Item label="用户名" name="username">
+            <Form {...layout} onFinish={handleOnFish}>
+                <Form.Item label="用户名" name="name">
                     <Input></Input>
                 </Form.Item>
                 <Form.Item label="密码" name="password">
