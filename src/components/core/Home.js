607e300cfdbc4c542a0f361a628ea586af103dc5
askwuxue <972523419@qq.com>
2021-06-16 22:33:37 +0800

    import SearchBar component and render that

--- src/components/core/Home.js
+++ src/components/core/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 // import { useSelector } from 'react-redux';
 import Layout from './Layout'
+import SearchBar from '../core/SearchBar';
 
 export default function Home() {
     // TODO useSelector
     return (
         <Layout title="精华书城" subTitle="好书尽在精华书城">
+            <SearchBar></SearchBar>
         </Layout >
     )
 }
