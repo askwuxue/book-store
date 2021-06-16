f0b8ab3a38a1e1aa7ace668693238e0bdd35d106
askwuxue <972523419@qq.com>
2021-06-16 22:35:04 +0800

    create common function

---
+++ src/utils/getCategories.js
@@ -0,0 +1,6 @@
+// // 请求后端获取分类数据
+// const getCategories = async () => {
+//     let { data: categories } = await axios.get(`${API}/categories`);
+//     setCategories(categories);
+// };
+// getCategories();
