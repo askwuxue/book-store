8b33e14b0c678ed3b5fb01c3fd11a3c85d3067bc
askwuxue <972523419@qq.com>
2021-06-18 00:27:33 +0800

    render server data

--- src/components/core/showProducts/LatestProduct.js
+++ src/components/core/showProducts/LatestProduct.js
@@ -1,26 +1,27 @@
 import React from 'react'
 import { Divider, Row, Col, Typography } from 'antd';
 import ProductItem from '../ProductItem';
+import { useSelector } from 'react-redux';
 const { Title } = Typography;
 
 export default function LatestProduct() {
+    // const product = useSelector(state => state.product)
+    // const { sold, createdAt } = useSelector(state => state.product)
+    const { sold } = useSelector(state => state.product)
+    // console.log('createdAt: ', createdAt);
+    // console.log('sold: ', sold);
     return (
         <>
             <Divider />
             <Title level={3}>最新上架</Title>
             <Row>
-                <Col span={6}>
-                    <ProductItem></ProductItem>
-                </Col>
-                <Col span={6}>
-                    <ProductItem></ProductItem>
-                </Col>
-                <Col span={6}>
-                    <ProductItem></ProductItem>
-                </Col>
-                <Col span={6}>
-                    <ProductItem></ProductItem>
-                </Col>
+                {
+                    sold.map(item => (
+                        <Col span={6} key={item._id}>
+                            <ProductItem></ProductItem>
+                        </Col>
+                    ))
+                }
             </Row>
         </>
     )
--- src/components/core/showProducts/MostPopular.js
+++ src/components/core/showProducts/MostPopular.js
@@ -1,26 +1,23 @@
 import React from 'react'
 import { Divider, Row, Col, Typography } from 'antd';
 import ProductItem from '../ProductItem';
+import { useSelector } from 'react-redux';
 const { Title } = Typography;
 
 export default function MostPopular() {
+    const { createdAt } = useSelector(state => state.product);
     return (
         <>
             <Divider />
             <Title level={3}>最受欢迎</Title>
             <Row>
-                <Col span={6}>
-                    <ProductItem></ProductItem>
-                </Col>
-                <Col span={6}>
-                    <ProductItem></ProductItem>
-                </Col>
-                <Col span={6}>
-                    <ProductItem></ProductItem>
-                </Col>
-                <Col span={6}>
-                    <ProductItem></ProductItem>
-                </Col>
+                {
+                    createdAt.map(item => (
+                        <Col span={6} key={item._id}>
+                            <ProductItem></ProductItem>
+                        </Col>
+                    ))
+                }
             </Row>
         </>
     )
