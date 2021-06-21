9e677c825827f3eba0ab76c776893fbecb312e14
askwuxue <972523419@qq.com>
2021-06-21 23:58:17 +0800

    import imgs

--- src/components/core/ProductItem.js
+++ src/components/core/ProductItem.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Row, Col, Card, Typography, Button } from 'antd';
 import { Link } from 'react-router-dom';
+import { API } from '../../../src/config';
 const { Title, Paragraph } = Typography;
 
 export default function ProductItem(props) {
-    let { name, description, price, quantity, createdAt } = props;
+    let { name, description, price, quantity, createdAt, _id } = props;
     return (
         <>
             <Card
                 style={{ width: 240 }}
-                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                cover={<img alt="example" src={`${API}/product/photo/${_id}`} />}
                 actions={[
                     <Button type="link"><Link to="/">查看详情</Link></Button>,
                     <Button type="link">加入购物车</Button>
