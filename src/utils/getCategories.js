b8109287f223b387f64ee6ddce1708f3e300d1d6
askwuxue <972523419@qq.com>
2021-06-21 23:57:41 +0800

    append define hook

--- src/utils/getCategories.js
+++
@@ -1,6 +0,0 @@
-// // 请求后端获取分类数据
-// const getCategories = async () => {
-//     let { data: categories } = await axios.get(`${API}/categories`);
-//     setCategories(categories);
-// };
-// getCategories();
