bca2272f98284e6e071d1bc22ae125d331aeeaf8
askwuxue <972523419@qq.com>
2021-06-22 23:33:10 +0800

    use search reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 import { connectRouter } from 'connected-react-router'
 import { registerReducer } from "./register";
 import { productReducer } from './product'
+import { searchReducer } from "./search";
 
 
 // 创建一个reducer
@@ -15,7 +16,8 @@ const createRootReducer = (history) => combineReducers({
     //connectRouter是一个函数，返回一个函数，该函数作为redux仓库中路由数据的reducer，进行更新仓库操作
     router: connectRouter(history),
     register: registerReducer,
-    product: productReducer
+    product: productReducer,
+    search: searchReducer
 })
 
 export default createRootReducer;
