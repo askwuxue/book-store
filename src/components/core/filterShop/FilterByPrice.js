617b8afaf564250be353d68531d5314685625870
askwuxue <972523419@qq.com>
2021-06-24 23:20:15 +0800

    show price selector component

--- src/components/core/filterShop/FilterByPrice.js
+++ src/components/core/filterShop/FilterByPrice.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { List, Typography, Radio } from 'antd';
+import { price } from '../../../utils/price';
 
-export default function FilterByPrice() {
+const { Title } = Typography;
+
+export default function FilterByPrice({ handleFilters }) {
+    // 获取用户输入的数据
+    const onChange = e => handleFilters(e.target.value);
     return (
-        <div>
-
-        </div>
+        <>
+            <Radio.Group onChange={onChange}>
+                <Title level={5}>按照分类筛选</Title>
+                <List
+                    dataSource={price}
+                    renderItem={item => (
+                        <List.Item>
+                            <Radio value={item.price}>
+                                {item.name}
+                            </Radio>
+                        </List.Item>
+                    )}
+                />
+            </Radio.Group>
+        </>
     )
 }
