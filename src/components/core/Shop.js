bb4d7ee625ffd50cd16b5197388f50fda8b546de
askwuxue <972523419@qq.com>
2021-06-24 23:48:54 +0800

    add loadMore logic

--- src/components/core/Shop.js
+++ src/components/core/Shop.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Layout from './Layout';
-import { Row, Col, Space } from 'antd';
+import { Row, Col, Space, Button, Empty } from 'antd';
 import FilterByCategory from './filterShop/FilterByCategory';
 import FilterByPrice from './filterShop/FilterByPrice';
 import { useDispatch, useSelector } from 'react-redux';
@@ -11,10 +11,22 @@ export default function Shop() {
     // filter数据获取
     const [filters, setFilter] = useState({ category: [], price: [] });
     const dispatch = useDispatch();
-    const filterData = useSelector(state => state.filter);
+    const { data, size } = useSelector(state => state.filter);
+    const [skip, setSkip] = useState(0);
+
+    // skip和filter发生变化，重新调用action
     useEffect(() => {
-        dispatch(filterProduct({ skip: 0, filters }))
-    }, [filters, dispatch])
+        dispatch(filterProduct({ skip, filters }))
+    }, [filters, skip, dispatch])
+
+    // 删选条件发生变化，skip置零
+    useEffect(() => {
+        setSkip(0);
+    }, [filters])
+
+    const loadMore = () => {
+        setSkip(skip + 4);
+    }
 
     return (
         <Layout title="书城列表" subTitle="挑选一本好书">
@@ -29,15 +41,22 @@ export default function Shop() {
                     </Space>
                 </Col>
                 <Col span={20}>
+                    {/* <Space size="large" direction="vertical"> */}
                     <Row gutter={[16, 16]}>
                         {
-                            filterData.data.map(item =>
+                            data.map(item =>
                                 <Col span="6" key={item._id}>
                                     <ProductItem {...item} />
                                 </Col>
                             )
                         }
                     </Row>
+                    <Row>
+                        {
+                            size >= 4 ? <Button onClick={loadMore}>加载更多</Button> : <Empty></Empty>
+                        }
+                    </Row>
+                    {/* </Space> */}
                 </Col>
             </Row>
         </Layout>
