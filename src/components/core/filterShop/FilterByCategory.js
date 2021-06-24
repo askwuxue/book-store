01629a359941a5c7a321d973dfa0c717d380633e
askwuxue <972523419@qq.com>
2021-06-24 23:19:47 +0800

    show category selector component

--- src/components/core/filterShop/FilterByCategory.js
+++ src/components/core/filterShop/FilterByCategory.js
@@ -4,21 +4,25 @@ import { List, Typography, Checkbox } from 'antd';
 
 const { Title } = Typography;
 
-export default function FilterByCategory() {
+export default function FilterByCategory({ handleFilters }) {
     const categories = useGetCategories();
+    // 获取CheckBox选择的值
+    const onChange = checkedValue => handleFilters(checkedValue);
     return (
         <>
             <Title level={5}>按照分类筛选</Title>
-            <List
-                dataSource={categories}
-                renderItem={item => (
-                    <List.Item>
-                        <Checkbox>
-                            {item.name}
-                        </Checkbox>
-                    </List.Item>
-                )}
-            />
+            <Checkbox.Group onChange={onChange}>
+                <List
+                    dataSource={categories}
+                    renderItem={item => (
+                        <List.Item>
+                            <Checkbox value={item._id}>
+                                {item.name}
+                            </Checkbox>
+                        </List.Item>
+                    )}
+                />
+            </Checkbox.Group>
         </>
     )
 }
